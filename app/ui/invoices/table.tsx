46dd4d37d323a6dbbbbd4a614b4e7f3ec618e190
wishwadev129 <wishwa.websrus@gmail.com>
2025-01-21 07:57:48 +0530

    working version for backup

--- app/ui/invoices/table.tsx
+++ app/ui/invoices/table.tsx
@@ -11,6 +11,7 @@ export default async function InvoicesTable({
   query: string;
   currentPage: number;
 }) {
+  console.log('here',query,"this", currentPage);
   const invoices = await fetchFilteredInvoices(query, currentPage);
 
   return (
